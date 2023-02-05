f7004c69ef732f0cf8681c106452878ab1743c92
charlie-wt <cwt1g14@soton.ac.uk>
2023-02-05 11:49:29 +0000

    tree sprite animation hookup

--- roots/tree_spot.lua
+++ roots/tree_spot.lua
@@ -1,3 +1,4 @@
+require "sprite"
 require "utils"
 require "roots.node"
 
@@ -5,10 +6,17 @@ TreeSpot = {
     RADIUS = 16,
     TIME = 5,
 
+    SPRITES = sprite.make_set("assets/tree/", { "tree 1.png", "tree 2.png", "tree 3.png" }),
+
+    GROW_DURATION = 2,
+    WITHER_DURATION = 1,
+
     x = nil,
     y = nil,
     node = nil,
     roots = nil,
+    t_grown = never,
+    t_cut = never,
 }
 setup_class("TreeSpot")
 
@@ -34,15 +42,62 @@ end
 function TreeSpot:update(dt)
     if self.node ~= nil and self.node.is_dead then
         self.node = nil
+        self.t_grown = never
+        self.t_cut = t
     end
+
+    if self.node ~= nil and self.t_grown == never then
+        self.t_grown = t
+        self.t_cut = never
+    end
+end
+
+function TreeSpot:age()
+    if self.t_grown == never then
+        return 0
+    end
+    return t - self.t_grown
+end
+
+function TreeSpot:since_cut()
+    if self.t_cut == never then
+        return 0
+    end
+    return t - self.t_cut
+end
+
+function TreeSpot:sprite()
+    if self.node ~= nil and self.t_grown ~= never then
+        return sprite.sequence(TreeSpot.SPRITES, TreeSpot.GROW_DURATION, self.t_grown)
+    end
+
+    if self.t_cut ~= never and self:since_cut() < TreeSpot.WITHER_DURATION then
+        return sprite.sequence(reverse(TreeSpot.SPRITES), TreeSpot.WITHER_DURATION, self.t_cut)
+    end
+
+    return nil
 end
 
 function TreeSpot:draw()
     love.graphics.setLineWidth(1)
     love.graphics.setColor({0.2, 0.4, 0, 0.2})
     love.graphics.circle("line", self.x, self.y, TreeSpot.RADIUS)
-    if self.node ~= nil then
+
+    local sprite = self:sprite()
+    if sprite == nil then
+        -- once we have a 'not-yet-grown-patch' sprite, this should never happen
         love.graphics.setColor({0.2, 0.4, 0, 1})
         love.graphics.circle("fill", self.x, self.y, TreeSpot.RADIUS * 0.5)
+        return
     end
+
+    local ox = sprite:getWidth() / 2
+    local oy = sprite:getHeight() / 2
+
+    -- want to draw the sprite slightly above centre, since the 'ground' bit is near
+    -- the bottom
+    oy = oy + 5
+
+    love.graphics.setColor({1, 1, 1, 1})
+    love.graphics.draw(sprite, self.x, self.y, 0, 1, 1, ox, oy)
 end
