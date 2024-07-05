48c82351294d762abe1eae9e0468c5304a0111f1
Alfie Edwards <alfie-edwards@hotmail.com>
2024-07-05 14:38:26 +0100

    Reducing latency wait

--- screens/game.lua
+++ screens/game.lua
@@ -9,7 +9,7 @@ Game = {
     MODE_ROOTS = {},
     MODE_ALL = {},
     INPUT_DELAY_S = 0.1,
-    LATENCY_SYNC_THRESHOLD_S = 0.3,
+    LATENCY_SYNC_THRESHOLD_S = 0,
 }
 setup_class(Game, LayoutElement)
 
