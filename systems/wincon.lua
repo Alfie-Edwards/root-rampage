a697409d9e117663d34305ee5161303affadf131
Alfie Edwards <alfie-edwards@hotmail.com>
2024-12-11 17:19:17 +0000

    Fix roots win bug

--- systems/wincon.lua
+++ systems/wincon.lua
@@ -21,7 +21,7 @@ function WINCON.update(state, inputs)
 
     if state.door.is_open then
         local door_pos = DOOR.get_center(state.door)
-        local most_recent_node = state.newest_node
+        local most_recent_node = NODE.from_id(state, state.newest_node)
         if most_recent_node ~= nil and (door_pos.x - most_recent_node.x) ^ 2 + (door_pos.y - most_recent_node.y) ^ 2 < (1.5 * LEVEL.cell_size() * LEVEL.cell_size()) then
             WINCON.RootsWin(wincon)
         end
